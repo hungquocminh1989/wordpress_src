8d3e7a9800f56c6b72e30ffeb906829c3fcb2b12
Minh <hungquocminh1989@gmail.com>
2019-06-18 11:03:49 +0700

    Update structure

--- wp-content/themes/storeshop-01/core/init.php
+++ wp-content/themes/storeshop-01/core/init.php
@@ -31,23 +31,23 @@ locate_template('/core/classes/class-tgm-plugin-activation.php', TRUE);
 | LOAD SETTINGS
 |-------------------------------------------------------------------------- 
 */
-locate_template('/core/settings/ajax-request.php', TRUE);
-locate_template('/core/settings/ajax-setting.php', TRUE);
-locate_template('/core/settings/client-side-script.php', TRUE);
-locate_template('/core/settings/default-posts-setting.php', TRUE);
-locate_template('/core/settings/default-pages-setting.php', TRUE);
-locate_template('/core/settings/customize-setting.php', TRUE);
-locate_template('/core/settings/metabox-setting.php', TRUE);
-locate_template('/core/settings/navigation-menus-setting.php', TRUE);
-locate_template('/core/settings/sidebars-setting.php', TRUE);
-locate_template('/core/settings/theme-setting.php', TRUE);
-locate_template('/core/settings/widgets-setting.php', TRUE);
-locate_template('/core/settings/post-type-setting.php', TRUE);
-locate_template('/core/settings/plugins-setting.php', TRUE);
-locate_template('/core/settings/admin-setting.php', TRUE);
-locate_template('/core/settings/api-setting.php', TRUE);
-locate_template('/core/settings/taxonomy-setting.php', TRUE);
-locate_template('/core/settings/shortcode-setting.php', TRUE);
+locate_template('/core/settings/wp/wp-ajax-request.php', TRUE);
+locate_template('/core/settings/wp/wp-ajax.php', TRUE);
+locate_template('/core/settings/wp/wp-client-side-script.php', TRUE);
+locate_template('/core/settings/wp/wp-default-posts.php', TRUE);
+locate_template('/core/settings/wp/wp-default-pages.php', TRUE);
+locate_template('/core/settings/wp/wp-customize.php', TRUE);
+locate_template('/core/settings/wp/wp-metabox.php', TRUE);
+locate_template('/core/settings/wp/wp-navigation-menus.php', TRUE);
+locate_template('/core/settings/wp/wp-sidebars.php', TRUE);
+locate_template('/core/settings/wp/wp-theme.php', TRUE);
+locate_template('/core/settings/wp/wp-widgets.php', TRUE);
+locate_template('/core/settings/wp/wp-post-type.php', TRUE);
+locate_template('/core/settings/wp/wp-plugins.php', TRUE);
+locate_template('/core/settings/wp/wp-admin.php', TRUE);
+locate_template('/core/settings/wp/wp-api.php', TRUE);
+locate_template('/core/settings/wp/wp-taxonomy.php', TRUE);
+locate_template('/core/settings/wp/wp-shortcode.php', TRUE);
 
 /*
 |--------------------------------------------------------------------------
