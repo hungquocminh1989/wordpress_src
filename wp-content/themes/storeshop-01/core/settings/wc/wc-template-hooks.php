94312370e37b4906483577f161b762726f4eeca9
Minh-Home <hungquocminh1989@gmail.com>
2019-07-07 22:43:32 +0700

    update

--- wp-content/themes/storeshop-01/core/settings/wc/wc-template-hooks.php
+++ wp-content/themes/storeshop-01/core/settings/wc/wc-template-hooks.php
@@ -9,7 +9,11 @@ defined( 'ABSPATH' ) || exit;
 /**
 * Hooks
 */
-add_action('storeshop01_sidebar','woocommerce_get_sidebar');
+add_action('storeshop_loop_item_image','woocommerce_template_loop_product_thumbnail');
+add_action('storeshop_loop_item_title','woocommerce_template_loop_product_title');
+add_action('storeshop_loop_item_price','woocommerce_template_loop_price');
+//remove_action();
+/*add_action('storeshop01_sidebar','woocommerce_get_sidebar');
 add_action('storeshop01_right_sidebar','get_product_right_sidebar');
 add_action('storeshop01_loop_product_thumbnail','woocommerce_template_loop_product_thumbnail');
 add_action('storeshop01_loop_product_title','get_loop_product_title');
@@ -21,13 +25,13 @@ add_action('storeshop01_single_product_title','woocommerce_template_single_title
 add_action('storeshop01_single_product_price','woocommerce_template_single_price');
 add_action('storeshop01_single_product_description','get_single_product_description');
 add_action('storeshop01_single_product_add_to_cart','woocommerce_template_single_add_to_cart');
-add_action('storeshop01_single_product_short_description','get_single_product_short_description');
+add_action('storeshop01_single_product_short_description','get_single_product_short_description');*/
 
 /**
 * Filters
 */
-add_filter('single_product_archive_thumbnail_size','apply_single_product_thumbnail_size');
+/*add_filter('single_product_archive_thumbnail_size','apply_single_product_thumbnail_size');
 add_filter('woocommerce_breadcrumb_defaults','apply_woocommerce_breadcrumb_defaults');
 add_filter('woocommerce_single_product_image_thumbnail_html','apply_product_image_thumbnail_html');
 //add_filter('woocommerce_gallery_thumbnail_size','repo_gallery_thumbnail_size');
-add_filter('woocommerce_checkout_fields','apply_custome_fields_billing');
+add_filter('woocommerce_checkout_fields','apply_custome_fields_billing');*/
---
+++ wp-content/themes/storeshop-01/woocommerce/content-product.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying product content within loops
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/content-product.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see     https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce/Templates
+ * @version 3.6.0
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+global $product;
+
+// Ensure visibility.
+if ( empty( $product ) || ! $product->is_visible() ) {
+	return;
+}
+?>
+<div class="col-md-4 product-men mt-5">
+	<div class="men-pro-item simpleCart_shelfItem">
+		<div class="men-thumb-item text-center">
+			<?php
+			do_action( 'storeshop_loop_item_image' );
+			?>
+			<!-- <div class="men-cart-pro">
+				<div class="inner-men-cart-pro">
+					<a href="single.html" class="link-product-add-cart">Quick View</a>
+				</div>
+			</div> -->
+		</div>
+		<div class="item-info-product text-center border-top mt-4">
+			<?php
+			/**
+			 * Hook: woocommerce_shop_loop_item_title.
+			 *
+			 * @hooked woocommerce_template_loop_product_title - 10
+			 */
+			do_action( 'storeshop_loop_item_title' );
+			?>
+			<div class="info-product-price my-2">
+				<?php
+				do_action( 'storeshop_loop_item_price' );
+				?>
+			</div>
+			<div class="snipcart-details top_brand_home_details item_add single-item hvr-outline-out">
+				<form action="#" method="post">
+					<fieldset>
+						<input type="hidden" name="cmd" value="_cart" />
+						<input type="hidden" name="add" value="1" />
+						<input type="hidden" name="business" value=" " />
+						<input type="hidden" name="item_name" value="Samsung Galaxy J7" />
+						<input type="hidden" name="amount" value="200.00" />
+						<input type="hidden" name="discount_amount" value="1.00" />
+						<input type="hidden" name="currency_code" value="USD" />
+						<input type="hidden" name="return" value=" " />
+						<input type="hidden" name="cancel_return" value=" " />
+						<input type="submit" name="submit" value="Add to cart" class="button btn" />
+					</fieldset>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
